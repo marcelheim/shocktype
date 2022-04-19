9a27bc369448414858a7efc7791344c91c254660
Marcel Heim <40496374+marcelheim@users.noreply.github.com>
2022-04-19 12:42:52 +0200

    add features

--- browser/shocktype.js
+++ browser/shocktype.js
@@ -1,16 +1,26 @@
+const arm = async () => {
+    console.log('armed')
+}
+
 const shock = async () => {
-    let host = await browser.storage.sync.get('host')
-    fetch(host.host)
+    //let host = await browser.storage.sync.get('host')
+    //fetch(host.host)
+    console.log('shock')
 }
 
 window.addEventListener('load', () => {
     const notificationCenterNode = document.getElementById('result');
-    const config = { childList: true, subtree: true };
+    const config = { childList: true, subtree: true, attributes: true };
+
+    arm();
 
     const notificationCenterMutationCallback = (mutationList, observer) => {
         for(let mutation of mutationList){
             if(mutation.type === 'childList' && mutation.addedNodes.length > 0){
-                if(mutation.addedNodes.length > 0 & mutation.addedNodes[0].data == 'failed (difficulty)') shock();
+                if(mutation.addedNodes.length > 0 & mutation.addedNodes[0].data.startsWith('failed')) shock();
+            }
+            if(mutation.type === 'attributes' && mutation.attributeName == 'class'){
+                if(mutation.target.attributes.class.nodeValue == 'hidden') arm();
             }
         }
     }
